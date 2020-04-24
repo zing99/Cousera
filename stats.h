e58b404edc5fa38dddc1a4f423e5f7d2ed3da16a
Sushrut Manu Singh <sushrutsingh1707@gmail.com>
2020-04-24 17:36:28 +0530

    Adding code outline and documentation for C1M1 Assessment

--- stats.h
+++ stats.h
@@ -8,33 +8,98 @@
  * misuse of this material. 
  *
  *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+/*
+ * @file <stats.h> 
+ * @brief <Header files for stats.c >
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Sushrut Manu Singh
+ * @date 24.04.2020
  *
  */
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
-/**
- * @brief <Add Brief Description of Function Here>
+void print_statistics(int min, int max, float mea, float med);
+/* 
+ * @breif Prints statistics of the array and the values 
+ *        calculated.
+ * 
+ *  Prints the value of minimun, maximum, mean and median
+ *  of the array by taking their values as input from the
+ *  other fucntions. 
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
  */
 
+void print_array(unsigned char arr[]);
+/*
+ * @breif Prints the array provided.
+ *
+ * Takes the value of the the array provided with question
+ * and prints its value by using a simple for loop fucntion.
+ *
+ * @param arr[]: Array to be printed
+ *
+ */
 
-#endif /* __STATS_H__ */
+float find_mean(unsigned char arr[], int length);
+/*
+ * @breif Calculates mean of array
+ *
+ * Takes the value of the array provided and find its mean
+ * value by adding all numbers and dividing it by length.
+ *
+ * @param arr[], length: length of array
+ *
+ */
+
+float find_median(unsigned char arr[],int length);
+/*
+ * @brief Calculates median of array
+ *
+ * Divides the value of lenth by 2 and finds the middle values 
+ * of the array. Returns two values for even value of length and
+ * one value for odd value of length.
+ *
+ * @param arr[], length
+ *
+ */
+
+int find_maximum(unsigned char arr[], int length);
+/*
+ * @breif Finds maximum value among the values of the array
+ *
+ * Takes the provided value of the array and then passes it
+ * through the sort array function and then returns the first
+ * value of the sorted array.
+ *
+ * @params arr[], lenght
+ *
+ */
+
+int find_minimum(unsigned char arr[], int length);
+/* 
+ * @brief Finds minimum value among the values of the array
+ *
+ * Takes the value of the given array and then passes it 
+ * through the sort array fuction and then returns the last
+ * element of the sorted array.
+ *
+ * @params arr[], length
+ *
+ */
+
+void sort_array(unsigned char arr[], int length);
+/*
+ * @breif Sorts the given array in descending order.
+ *
+ * Takes the given array and sorts it in descending order
+ * Then stores the sorted array in old variable itself.
+ *
+ * @params arr[], length
+ *
+ */
+
+endif /* __STATS_H__*/
+
